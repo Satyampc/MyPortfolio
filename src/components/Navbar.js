0965612f971b7b80e20b229646b2ce62cc8fdafe
Satyampc <40965826+Satyampc@users.noreply.github.com>
2020-10-12 12:03:03 -0400

    Home Path corrected

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,7 @@ const menuItems = [
     {
         listIcon: <Home />,
         listText: "Home",
-        listPath: "/"
+        listPath: "/portfolio"
     },
     {
         listIcon: <AssignmentInd />,
